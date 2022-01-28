4ed67d4e0c5d727ac489179e2e0aa1cfdd365719
seleznevme <seleznevme@gmail.com>
2022-01-28 16:31:38 +0200

    поправил кнопку добавить файл

--- src/components/UI/AddFile.vue
+++ src/components/UI/AddFile.vue
@@ -1,17 +1,76 @@
 <template>
-  <input class="btn_add_file" type="file"/>
+  <nav-button>
+    <div class="input__wrapper">
+      <input
+        name="file"
+        type="file"
+        id="input__file"
+        class="input input__file"
+      />
+      <label for="input__file" class="input__file-button">
+        <span class="input__file-icon-wrapper"
+          ><img
+            class="input__file-icon"
+            src="./img/add.png"
+            alt="Выбрать файл"
+            width="25"
+        /></span>
+        <span class="input__file-button-text">Выберите файл</span>
+      </label>
+    </div>
+  </nav-button>
 </template>
 
 <script>
 export default {
-    name: "add-file",
-    props: {
-       
-    }          
-}
+  name: "add-file",
+  props: {},
+};
 </script>
 
 <style  scoped>
+.input__wrapper {
+  position: relative;
+  text-align: center;
+}
 
+.input__file {
+  opacity: 0;
+  visibility: hidden;
+  position: absolute;
+}
 
+.input__file-icon-wrapper {
+  height: 30px;
+  width: 30px;
+  margin-right: 10px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+}
+
+.input__file-button-text {
+  line-height: 1;
+  margin-top: 1px;
+}
+
+.input__file-button {   
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: start;
+  -ms-flex-pack: start;
+  justify-content: flex-start;  
+  cursor: pointer;
+  margin: 0 auto;
+}
 </style>
